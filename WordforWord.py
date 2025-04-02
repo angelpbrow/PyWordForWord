93dabbd397acfb519f75a49c748cefa54847174a
Angelika Brown <angelikpbrown@gmail.com>
2025-04-02 14:28:57 -0400

    Filing it up

---
+++ WordforWord.py
@@ -0,0 +1,28 @@
+import os
+import re
+
+class wordProcessor:
+
+    pass
+
+    def readfile(self, file_path):
+        with open(file_path) as f:
+            content = f.read()
+            f.close()
+        return content
+
+
+    def wc(self):
+        pass
+
+
+    def wordFrequency(self):
+        pass
+
+
+    def letterFrequency(self):
+        pass
+
+
+
+
